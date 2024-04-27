e4282ce7776b3d3ebb44e56c1bd55d53fd60dad8
jaks <jaks978@gmail.com>
2024-04-27 23:37:50 +0530

    vin list retrieved

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,33 @@ class Vehicle {
   }
 }
 
+class VehicleDetail {
+  final String createdDate;
+  final String id;
+  final String latitude;
+  final String longitude;
+
+  VehicleDetail({
+    required this.createdDate,
+    required this.id,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory VehicleDetail.fromJson(Map<String, dynamic> json) {
+    return VehicleDetail(
+      createdDate: json['createdDate'],
+      id: json['id'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+    );
+  }
+}
+
 class VehicleController extends GetxController {
   var vehicleList = <Vehicle>[].obs;
   var selectedVehicle = ''.obs;
+  var vehicleDetail = <VehicleDetail>[].obs;
 
   @override
   void onInit() {
@@ -49,8 +73,29 @@ class VehicleController extends GetxController {
     }
   }
 
+  Future<void> fetchVehicleDetail(String vin) async {
+    try {
+      final response = await http.get(Uri.parse(
+          'https://p1q2az2mxk.execute-api.ap-south-1.amazonaws.com/production/xyz_list_item_vin?VIN=$vin'));
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        List<dynamic> vehicleDetailsData = jsonData;
+
+        vehicleDetail.value = vehicleDetailsData
+            .map((json) => VehicleDetail.fromJson(json))
+            .toList();
+      } else {
+        throw Exception('Failed to load vehicle detail: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to load vehicle detail: $e');
+    }
+  }
+
   void selectVehicle(String vehicleId) {
     selectedVehicle.value = vehicleId;
+    fetchVehicleDetail(vehicleId);
   }
 }
 
@@ -69,26 +114,53 @@ class MyApp extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(
-              child: Obx(
-                    () => ListView.builder(
-                  itemCount: vehicleController.vehicleList.length,
-                  itemBuilder: (context, index) {
-                    final vehicle = vehicleController.vehicleList[index];
-                    return ListTile(
-                      title: Text('VIN: ${vehicle.vin}'),
-                      onTap: () => vehicleController.selectVehicle(vehicle.vin),
-                    );
-                  },
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Obx(
+                      () => ListView.builder(
+                    itemCount: vehicleController.vehicleList.length,
+                    itemBuilder: (context, index) {
+                      final vehicle = vehicleController.vehicleList[index];
+                      return ListTile(
+                        title: Text('${vehicle.vin}'),
+                        onTap: () => vehicleController.selectVehicle(vehicle.vin),
+                      );
+                    },
+                  ),
                 ),
               ),
             ),
             Expanded(
-              child: Obx(
-                    () => Text(
-                  'Selected Vehicle VIN: ${vehicleController.selectedVehicle}',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ),
+              flex: 8,
+              child: Column(
+                children:[
+                  Text(
+                    'selected vin : '
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Obx(
+                          () => DataTable(
+                        columns: [
+                          DataColumn(label: Text('Created Date')),
+                          DataColumn(label: Text('ID')),
+                          DataColumn(label: Text('Latitude')),
+                          DataColumn(label: Text('Longitude')),
+                        ],
+                        rows: vehicleController.vehicleDetail.map((vehicle) {
+                          return DataRow(cells: [
+                            DataCell(Text(vehicle.createdDate)),
+                            DataCell(Text(vehicle.id)),
+                            DataCell(Text(vehicle.latitude)),
+                            DataCell(Text(vehicle.longitude)),
+                          ]);
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                ]
+              )
             ),
           ],
         ),
